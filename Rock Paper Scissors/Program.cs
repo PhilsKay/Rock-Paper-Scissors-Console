e7d4cae960bdf8b8e0611ed8065946f31c7e8edc
danikay tech <danikay tech@LAPTOP-3J25IRPG>
2021-08-21 16:22:50 +0100

    Add project files.

---
+++ Rock Paper Scissors/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playAgain = " ";
+            do
+            {
+                TheGame();
+                Console.WriteLine("Enter \"P\" to play again or any other key to quit");
+                playAgain = Console.ReadLine().ToUpper();
+            } while (playAgain == "P");
+            
+            // the Rock Paper and scossors method
+            static void TheGame()
+            {
+                Random random = new Random();
+                int botChoice = random.Next(1, 3);
+                int userChoice = 0;
+                bool notCorrect = true;
+               // While the user does not choose the right case will continue.
+                while (notCorrect)
+            {
+                    try
+                    {
+                        Console.Write("\nSelect from 1 - 3 Rock (1), Paper (2), Scissors(3) : ");
+                        userChoice = int.Parse(Console.ReadLine());
+                        switch (userChoice)
+                        {
+                            case 1:
+                                Console.WriteLine("Rock");
+                                notCorrect = false;
+                                break;
+                            case 2:
+                                Console.WriteLine("Paper");
+                                notCorrect = false;
+                                break;
+                            case 3:
+                                Console.WriteLine("Scissors");
+                                notCorrect = false;
+                                break;
+                            default:
+                                Console.WriteLine("You picked the wrong tool");
+                                break;
+                        }
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message + "You picked the wrong tool");
+                    }
+                }
+                // To get the AI choice
+                switch (botChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Bot chose Rock");
+                        break;
+                    case 2:
+                        Console.WriteLine("Bot chose Paper");
+                        break;
+                    case 3:
+                        Console.WriteLine("Bot chose Scissors");
+                        break;
+                    default:
+                        break;
+                }
+                // Still gonna update the scores countint between the user and the AI Perfectly
+                // cause i feel like to add it.
+                int botScores = 0;
+                int userScores = 0;
+                    if (userChoice == 1 && botChoice == 3)
+                    {
+                        Console.WriteLine("You smash the Scissors \nYou Win!");
+                        userScores++;
+                    }
+                    else if (botChoice == 1 && userChoice == 3)
+                    {
+                        Console.WriteLine("Bot smashes your Scissors \nYou Lose!");
+                        botScores++;
+                    }
+                    else if (userChoice == 3 && botChoice == 2)
+                    {
+                        Console.WriteLine("You cut the Paper \nYou Win!");
+                        userScores++;
+                    }
+                    else if (botChoice == 3 && userChoice == 2)
+                    {
+                        Console.WriteLine("Bot cut your Paper \nYou Lose!");
+                        botScores++;
+                    }
+                    else if (userChoice == 2 && botChoice == 1)
+                    {
+                        Console.WriteLine("You wrap the Rock \nYou Win!");
+                        userScores++;
+                    }
+                    else if (botChoice == 2 && userChoice == 1)
+                    {
+                        Console.WriteLine("Bot wrasp your Rock \nYou Lose!");
+                        botScores++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("It's a Tie!");
+                    }
+                    Console.WriteLine("\nScores : You {0} - Bot {1}", userScores, botScores);
+            }
+            Console.ReadLine();
+        }
+    }
+}
